a233a1ef8ed7d2b264edcd1c0258156178d46679
tamaraporfiro <tamaraporfiro@gmail.com>
2024-09-16 10:46:00 -0300

    adc prompt template

--- langchain_main.py
+++ langchain_main.py
@@ -1,22 +1,36 @@
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
 
+# Load environment variables from .env file
 load_dotenv()
 
+
+# Define variables
 numero_de_dias = 7
 numero_de_criancas = 2
 atividade = "praia"
 
 
-prompt = f"Crie um roteiro de viagem de {numero_de_dias} dias, para uma familia com {numero_de_criancas} crianças, que gostam de {atividade}"
-print(prompt)
+# Define the prompt template and fill in the variables
+template = PromptTemplate.from_template(
+"Crie um roteiro de viagem de {dias} dias, para uma familia com {criancas} crianças, que gostam de {atividade}"
+)
 
+# Create the prompt with the filled-in variables
+prompt= template.format(dias=numero_de_dias,
+                criancas=numero_de_criancas,
+                atividade=atividade)
+
+# Create the LLM (Language Model)
 llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY") ,
                  model="gpt-3.5-turbo",
                  temperature=0.5)
 
-resposta = llm.invoke(prompt)
+# Use the LLM to generate the roteiro de viagem
+report = llm.invoke(prompt)
 
-print(resposta.content)
+# Print the generated report to the console
+print(report.content)
